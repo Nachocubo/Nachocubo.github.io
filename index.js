869569575d987bde2d43d07a42dd5a6c273f95ce
Nacho <66080289+Nachocubo@users.noreply.github.com>
2024-05-28 10:46:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,91 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const contentElement = document.getElementById("content");
     const languageSelect = document.getElementById("language-select");
+    let typeTimeout;
 
     // Función para cargar el archivo JSON
-    function loadJSON(url, callback) {   
+    function loadJSON(url, callback) {
         const xhr = new XMLHttpRequest();
         xhr.overrideMimeType("application/json");
-        // Añadimos un timestamp para evitar la caché del navegador
         const fullUrl = url + '?timestamp=' + new Date().getTime();
         xhr.open('GET', fullUrl, true);
         xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    callback(JSON.parse(xhr.responseText));
-                } else {
-                    console.error('Error al cargar el archivo JSON:', xhr.status, xhr.statusText);
-                }
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                callback(JSON.parse(xhr.responseText));
+            } else if (xhr.readyState === 4) {
+                console.error('Error al cargar el archivo JSON:', xhr.status, xhr.statusText);
             }
         };
-        xhr.send(null);  
+        xhr.send(null);
     }
 
     // Función para actualizar el contenido según el idioma seleccionado
     function updateContent(content) {
+        const typewriterElement = document.getElementById("typewriter");
+        let index = 0;
+        const text = content.tituloWriter;
+
         contentElement.innerHTML = `
-            <table border="1" style="border-collapse:collapse;">
+            <table border="1" style="border-collapse: collapse;">
                 <tr><th>Saludo</th><th>Despedida</th></tr>
-                <tr><td>${content.saludo} ${languageSelect[languageSelect.selectedIndex].innerText}</td><td>${content.despedida}</td></tr>
+                <tr><td>${content.saludo} ${languageSelect.options[languageSelect.selectedIndex].text}</td><td>${content.despedida}</td></tr>
             </table>`;
+
+        document.getElementById('separator-1').innerHTML = content.texto1;
+        document.getElementById('paragraph-1').innerHTML = content.parrafo1;
+        document.getElementById('paragraph-2').innerHTML = content.parrafo2;
+        document.getElementById('paragraph-3').innerHTML = content.parrafo3;
+        typewriterElement.innerHTML = '';
+        document.querySelector('h3').innerHTML = content.proyectos;
+
+        if (typeTimeout) {
+            clearTimeout(typeTimeout);
+        }
+
+        function type() {
+            if (index < text.length) {
+                typewriterElement.innerHTML += text[index];
+                index++;
+                typeTimeout = setTimeout(type, 100); // Guarda el nuevo timeout
+            } else {
+                typeTimeout = setTimeout(() => {
+                    index = 0;
+                    typewriterElement.innerHTML = '';
+                    typeTimeout = setTimeout(type, 100);
+                }, 5000);
+            }
+        }
+
+        type();
     }
 
-    // Llama a loadJSON() para cargar el contenido inicial desde el archivo JSON
-    loadJSON('indice.json', function(paths) {
-        // Llama a loadJSON() para cargar el contenido en el idioma seleccionado
-        loadJSON(paths[languageSelect.value], function(content) {
-            // Llama a updateContent() cuando cambia el valor de la lista desplegable
-            languageSelect.addEventListener("change", function() {
-                loadJSON(paths[languageSelect.value], function(content) {
-                    console.log(content);
-                    updateContent(content);
-                });
-            });
+    function saveLanguageSelection(language) {
+        localStorage.setItem('selectedLanguage', language);
+    }
 
-            // Actualiza el contenido inicialmente al cargar la página
-            updateContent(content);
+    function loadLanguageSelection() {
+        return localStorage.getItem('selectedLanguage');
+    }
+
+    // Carga inicial del archivo JSON
+    loadJSON('indice.json', function (paths) {
+        function loadContent() {
+            loadJSON(paths[languageSelect.value], function (content) {
+                updateContent(content);
+            });
+        }
+
+        // Establece el idioma seleccionado desde el localStorage si existe
+        const savedLanguage = loadLanguageSelection();
+        if (savedLanguage && languageSelect.querySelector(`option[value="${savedLanguage}"]`)) {
+            languageSelect.value = savedLanguage;
+        }
+
+        loadContent();
+
+        languageSelect.addEventListener("change", function () {
+            saveLanguageSelection(languageSelect.value);
+            loadContent();
         });
     });
-
-    const text = document.querySelector('.typewriter').innerText;
-    const typewriterElement = document.getElementById("typewriter");
-    let index = 0;
-    typewriterElement.textContent = '';
-
-    function type() {
-        if (index < text.length) {
-            typewriterElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 100);
-        } else {
-            setTimeout(() => {
-                index = 0;
-                typewriterElement.textContent = '';
-                setTimeout(type, 100);
-            }, 10000);
-        }
-    }
-
-    type();
 });
